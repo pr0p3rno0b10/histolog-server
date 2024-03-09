681d9a3d3757482c30e70a4efc4390cb453b7edd
Jack Nemtsov <jacknemtsov@Jacks-Air.lan>
2024-03-09 21:04:52 +0000

    Updated CORS

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://histolog-client.vercel.app/'
 ]
 
 CACHES = {
